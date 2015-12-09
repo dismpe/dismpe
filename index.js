28f1028354a49471f02148d542744ca8e5e72a5c
Yonggang Luo <luoyonggang@gmail.com>
2015-12-10 00:45:43 +0800

    rename it.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const start = require('./src/dsimpe.js').start;
 
 let options = {
   rootDir: __dirname,
-  osPath: 'Win7/en_windows_7_ultimate_with_sp1_x64_dvd_u_677332-backup.iso',
+  osPath: 'Win7/en_windows_7_ultimate_with_sp1_x64_dvd_u_677332.iso',
   patchPath: 'Win7/Windows-KB913086-201511.iso',
   forceMount: false,
   mountDir: 'C:/Win7/mount',
