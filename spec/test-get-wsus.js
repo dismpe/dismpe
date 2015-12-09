348cf14944aecf87f65d5ae29db3144d1800febe
Yonggang Luo <luoyonggang@gmail.com>
2015-12-10 00:05:47 +0800

    Well, split dsimpe.js out.

--- spec/test-get-wsus.js
+++ spec/test-get-wsus.js
@@ -1,14 +1,14 @@
 'use strict';
 
-let path = require('path');
-let expect = require('chai').expect;
+let path = require('path')
+let expect = require('chai').expect
+let dsimpe = require('../src/dsimpe.js')
 
-let index = require('../index');
+let getWsusPackagePaths = dsimpe.getWsusPackagePaths;
+let EXCLUDS_FOR_WIN7 = dsimpe.EXCLUDS_FOR_WIN7;
+let getWHDownloaderPackagePaths = dsimpe.getWHDownloaderPackagePaths;
 
-let getWsusPackagePaths = index.getWsusPackagePaths;
-let EXCLUDS_FOR_WIN7 = index.EXCLUDS_FOR_WIN7;
-
-describe('index#getWsusPackagePaths', function(){
+describe('dsimpe#', function(){
 
   let options = {
     rootDir: path.dirname(__dirname),
@@ -16,7 +16,7 @@ describe('index#getWsusPackagePaths', function(){
     lang: 'enu',
     ieLang: 'en-us',
   }
-  it('Should return the correct package list', function*(){
+  it('getWsusPackagePaths Should return the correct package list', function*(){
     this.timeout(10000);
     const packageList = yield* getWsusPackagePaths(options);
     expect(packageList.length).to.equal(192);
@@ -32,7 +32,7 @@ describe('index#getWsusPackagePaths', function(){
     }
     return notFiltered;
   }
-  it('Testing the exclude works', function*(){
+  it('getWsusPackagePaths Testing the exclude works', function*(){
     this.timeout(10000);
     expect(EXCLUDS_FOR_WIN7.indexOf('2506014')).to.equal(-1);
     const packageList = yield* getWsusPackagePaths(options);
@@ -40,5 +40,44 @@ describe('index#getWsusPackagePaths', function(){
     expect(notFiltered).to.eql(['2506014']);
     notFiltered = getNotFiltered(EXCLUDS_FOR_WIN7, packageList)
     expect(notFiltered).to.eql([]);
+  });
+
+  it('test path sort', function(){
+    this.timeout(10000);
+    let paths = ['Windows6.1-KB2857650-x64.msu', 'Windows6.1-KB2574819-v2-x64.msu'];
+    expect(paths.sort()).to.eql(['Windows6.1-KB2574819-v2-x64.msu', 'Windows6.1-KB2857650-x64.msu'])
+  });
+
+  it('getWHDownloaderPackagePaths', function(){
+    let ret = getWHDownloaderPackagePaths(options)
+    expect(ret).to.eql([
+      'WHDownloader\\Updates\\Windows7-x64\\General',
+      'WHDownloader\\Updates\\Windows7-x64\\Security',
+      'WHDownloader\\Updates\\Windows7-x64\\Hotfix',
+      'WHDownloader\\Updates\\Windows7-x64\\Additional\\_IE11',
+      'WHDownloader\\Updates\\Windows7-x64\\Additional\\_IE11\\Updates',
+      [ 'WHDownloader\\Updates\\Windows7-x64\\Additional\\Features\\IIS',
+        'WHDownloader\\Updates\\Windows7-x64\\Additional\\Features\\LPRPortMonitor',
+        'WHDownloader\\Updates\\Windows7-x64\\Additional\\Features\\MSMQ',
+        'WHDownloader\\Updates\\Windows7-x64\\Additional\\Features\\NFS',
+        'WHDownloader\\Updates\\Windows7-x64\\Additional\\Features\\SNMP',
+        'WHDownloader\\Updates\\Windows7-x64\\Additional\\Features\\SUA',
+        'WHDownloader\\Updates\\Windows7-x64\\Additional\\Features\\TelnetServer' ],
+      [ 'WHDownloader\\Updates\\Windows7-x64\\Additional\\RDP\\Windows6.1-KB2574819-v2-x64.msu',
+        'WHDownloader\\Updates\\Windows7-x64\\Additional\\RDP\\Windows6.1-KB2592687-x64.msu',
+        'WHDownloader\\Updates\\Windows7-x64\\Additional\\RDP\\Windows6.1-KB2830477-x64.msu',
+        'WHDownloader\\Updates\\Windows7-x64\\Additional\\RDP\\Windows6.1-KB2923545-x64.msu',
+        'WHDownloader\\Updates\\Windows7-x64\\Additional\\RDP\\Windows6.1-KB2984976-x64.msu',
+        'WHDownloader\\Updates\\Windows7-x64\\Additional\\RDP\\Windows6.1-KB2985461-x64.msu',
+        'WHDownloader\\Updates\\Windows7-x64\\Additional\\RDP\\Windows6.1-KB3020387-x64.msu',
+        'WHDownloader\\Updates\\Windows7-x64\\Additional\\RDP\\Windows6.1-KB3020388-x64.msu',
+        'WHDownloader\\Updates\\Windows7-x64\\Additional\\RDP\\Windows6.1-KB3024260-x64.msu',
+        'WHDownloader\\Updates\\Windows7-x64\\Additional\\RDP\\Windows6.1-KB3069762-x64.msu' ],
+      'WHDownloader\\Updates\\Windows7-x64\\Additional\\WAT',
+      'WHDownloader\\Updates\\Windows7-x64\\Extra\\MediaFeaturePack',
+      'WHDownloader\\Updates\\Windows7-x64\\Extra\\MediaFormatFeaturePack',
+      'WHDownloader\\Updates\\Windows7-x64\\Extra\\RSAT',
+      'WHDownloader\\Updates\\Windows7-x64\\Extra\\RSAT\\WMS',
+      'WHDownloader\\Updates\\Windows7-x64\\Extra\\RSAT\\Updates' ]);
   });
 })
